4ab753d4675de49cf634ff058ddf600f51a13beb
John Roijackers <john.roijackers@adversitement.com>
2017-03-15 09:34:13 +0100

    Start DimML with default configuration.

--- dimml.sh
+++ dimml.sh
@@ -1,8 +1,5 @@
-# Install Java
-sudo apt-get update
-sudo apt-get install -y openjdk-8-jdk
-
 # Install Docker
+sudo apt-get update
 sudo apt-get install \
     apt-transport-https \
     ca-certificates \
@@ -16,3 +13,19 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install -y docker-ce
 sudo systemctl enable docker
+
+# Import DimML files
+sudo apt-get install -y unzip
+mkdir --parents /home/dimml/src/dimml-docker
+cd /home/dimml/src/dimml-docker/
+wget --output-document dimml-docker.zip "https://dimmlarm.blob.core.windows.net/dimml-arm-template/dimml-docker.zip?st=2017-01-01T00%3A00%3A00Z&se=2018-01-01T00%3A00%3A00Z&sp=rl&sv=2015-12-11&sr=b&sig=foDZAfU04GU5IKlUWcW%2BYlIflsy7Mw6n7v8bNVNPa6k%3D"
+unzip dimml-docker.zip
+rm dimml-docker.zip
+
+# Create Docker image and container
+sudo /usr/bin/docker build --tag dimml-docker .
+chmod +x create.sh /home/dimml/src/dimml-docker/dimml/bin/dimml
+./create.sh
+
+# Start DimML
+sudo /usr/bin/docker start dimml
